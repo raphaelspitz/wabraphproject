b96748b197d4c614475176de18b61a088a862ea0
raphaelspitz <spitz.raphael@gmail.com>
2022-06-07 13:23:56 +0200

    toggle menu added

--- my-app/src/components/Filter.tsx
+++ my-app/src/components/Filter.tsx
@@ -4,6 +4,11 @@ type ChildProps  = {
   setInputValue: (arg: string) => void
 };
 const Filter: React.FC<ChildProps> = (props) => {
+  const iniTialClassNameDropDown = 'dropdown-menu border mt-0 position-relative top-0';
+  const [classNameDropDownMenu] = useState(iniTialClassNameDropDown);
+  const [isFilterBtnToggled, setIsFilterBtnToggled] = useState(false);
+  const toggleFilter = () => setIsFilterBtnToggled(value => !value);
+
   return (
     <div className="d-flex flex-column">
       <div className="d-flex mb-5">
@@ -11,10 +16,28 @@ const Filter: React.FC<ChildProps> = (props) => {
             <input type="Search" className="form-control" placeholder="Searcg" 
               onChange={(event)=>props.setInputValue(event.target.value)} />
         </div>
-        <div className="col-4">
-          <button className="btn btn-secondary dropdown-toggle w-100" type="button">
+        <div className="col-4 d-flex flex-column">
+          <div className={isFilterBtnToggled ? 'dropup' : ''}>
+          <button onClick={toggleFilter} className="btn btn-secondary dropdown-toggle w-100" type="button">
             Filter
           </button>
+          </div>
+          <div className={isFilterBtnToggled ? classNameDropDownMenu + ' show' : classNameDropDownMenu}>
+            <div className="form-check form-check-inline d-flex justify-content-between p-2">
+              <div>
+                <input className="form-check-input" type="checkbox" value="option1" />
+                <label className="form-check-label" htmlFor="inlineCheckbox1">France</label>
+              </div>
+              <div>
+                <input className="form-check-input" type="checkbox" value="option2" />
+                <label className="form-check-label" htmlFor="inlineCheckbox1">Italian</label>
+              </div>
+              <div>
+                <input className="form-check-input" type="checkbox" value="option3" />
+                <label className="form-check-label" htmlFor="inlineCheckbox1">UK</label>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
