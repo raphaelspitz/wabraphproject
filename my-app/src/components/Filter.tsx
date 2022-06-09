a12ba5a671fe5fef2dc04c883ae739baf922fb45
raphaelspitz <spitz.raphael@gmail.com>
2022-06-09 22:55:48 +0200

    filter finished and no more bugs

--- my-app/src/components/Filter.tsx
+++ my-app/src/components/Filter.tsx
@@ -1,20 +1,31 @@
 import React, { useState } from 'react';
 type ChildProps  = {
   children?: React.ReactNode,
-  setInputValue: (arg: string) => void
+  handleFilterChange: (arg: string, arg2: string) => void
 };
 const Filter: React.FC<ChildProps> = (props) => {
-  const iniTialClassNameDropDown = 'dropdown-menu border mt-0 position-relative top-0';
-  const [classNameDropDownMenu] = useState(iniTialClassNameDropDown);
+  const classNameDropDown = 'dropdown-menu border mt-0 position-relative top-0';
   const [isFilterBtnToggled, setIsFilterBtnToggled] = useState(false);
   const toggleFilter = () => setIsFilterBtnToggled(value => !value);
+  
+  const setInputValue = (event:any) => {
+    emitInputAndCheckboxes("input", event.target.value);
+  }
+  
+  const setCheckboxValue = (event:any) => {
+    emitInputAndCheckboxes("box", event.currentTarget.name);
+  }
+
+  const emitInputAndCheckboxes = (inputType:string, inpput:string) => {
+    props.handleFilterChange(inputType, inpput);
+  } 
 
   return (
     <div className="d-flex flex-column">
       <div className="d-flex mb-5">
         <div className="form-group col-8 p-0">
             <input type="Search" className="form-control" placeholder="Searcg" 
-              onChange={(event)=>props.setInputValue(event.target.value)} />
+              onChange={setInputValue} />
         </div>
         <div className="col-4 d-flex flex-column">
           <div className={isFilterBtnToggled ? 'dropup' : ''}>
@@ -22,18 +33,19 @@ const Filter: React.FC<ChildProps> = (props) => {
             Filter
           </button>
           </div>
-          <div className={isFilterBtnToggled ? classNameDropDownMenu + ' show' : classNameDropDownMenu}>
+          <div className={isFilterBtnToggled ? classNameDropDown + ' show' : classNameDropDown}>
             <div className="form-check form-check-inline d-flex justify-content-between p-2">
               <div>
-                <input className="form-check-input" type="checkbox" value="option1" />
+                <input name="France" onClick={setCheckboxValue}
+                   className="form-check-input" type="checkbox" />
                 <label className="form-check-label" htmlFor="inlineCheckbox1">France</label>
               </div>
               <div>
-                <input className="form-check-input" type="checkbox" value="option2" />
+                <input name="Italy" onClick={setCheckboxValue} className="form-check-input" type="checkbox" value="option2" />
                 <label className="form-check-label" htmlFor="inlineCheckbox1">Italian</label>
               </div>
               <div>
-                <input className="form-check-input" type="checkbox" value="option3" />
+                <input name="United Kingdom" onClick={setCheckboxValue} className="form-check-input" type="checkbox" value="option3" />
                 <label className="form-check-label" htmlFor="inlineCheckbox1">UK</label>
               </div>
             </div>
--- my-app/src/components/Home.tsx
+++ my-app/src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Filter from './Filter';
 import Table from './Table';
 import Books from '../mocks/books.json'
@@ -6,20 +6,60 @@ import Books from '../mocks/books.json'
 function Home() {
   const [books, setBooks] = useState(Books);
   const [booksTemp] = useState(Books);
+  const [inputText, setInputText] = useState<string>('');
+  const [checkboxes, setCheckboxes] = useState<string[]>([]);
 
-  const setInputValue = (name: string):void => {
-    let filter = booksTemp.filter(value => {
-     return value.author.includes(name);
-    });
-    
-    filter = filter ? filter : booksTemp;
-    setBooks(filter);
+  useEffect(() => {
+   let filter = booksTemp.filter(value => {
+     // si toutes les checkbox sont vide mais qu'on fait une recherche que avec input
+     if(checkboxes.length == 0 && inputText.length >= 0) {
+       return value.author.toLowerCase().includes(inputText.toLowerCase());
+     }
+    //ici on doit gérer si 1 ou plusieurs checkbox sont cliqués mais que input text est vide
+      if(checkboxes.length > 0 && inputText.length == 0) {
+        for (let index = 0; index < checkboxes.length; index++) {
+          if(value.country.toLowerCase() == checkboxes[index].toLowerCase())
+            return (value.country.toLowerCase() == checkboxes[index].toLowerCase());
+        }
+      } 
+      // si une ou plusieurs checkbox sont cliqué et que inputext est pas vide
+      if(checkboxes.length > 0 && inputText.length > 0)  {
+          for (let index = 0; index < checkboxes.length; index++) {
+            if(value.country.toLowerCase() == checkboxes[index].toLowerCase() && 
+            (value.author.toLowerCase().includes(inputText.toLowerCase()))
+            )
+              return (value.country.toLowerCase() == checkboxes[index].toLowerCase())
+              && (value.author.toLowerCase().includes(inputText.toLowerCase()))
+          }
+      }    
+  });
+  
+  filter = filter ? filter : booksTemp;
+  setBooks(filter);
+  
+  }, [checkboxes, inputText])
+
+  const handleFilterChange = (inputType: string, input:string):void => {
+    if(inputType == 'input') {
+      setInputText(input);
+    } else {
+        let index = checkboxes.indexOf(input);
+        if(index == -1) {
+          // addCheckbox
+          setCheckboxes([...checkboxes, input]);
+        } else {
+          //removeCheckbox
+            let array = [...checkboxes]; // make a separate copy of the array
+            array.splice(index, 1);
+            setCheckboxes(array);
+        }
+    }
   }
 
   return (
     <div className="Home">
       <div>
-        <Filter setInputValue={setInputValue} />
+        <Filter handleFilterChange={handleFilterChange} />
         <Table Books={books} />
       </div>
     </div>
