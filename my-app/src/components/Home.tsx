1f61c997a503212bcac6e35e4e1e41bf7ede16e7
raphaelspitz <spitz.raphael@gmail.com>
2022-06-10 18:40:51 +0200

    refactoring

--- my-app/src/components/Home.tsx
+++ my-app/src/components/Home.tsx
@@ -2,43 +2,64 @@ import React, { useEffect, useState } from 'react';
 import Filter from './Filter';
 import Table from './Table';
 import Books from '../mocks/books.json'
+import { Book } from './book';
+
 
 function Home() {
-  const [books, setBooks] = useState(Books);
-  const [booksTemp] = useState(Books);
+  const [books, setBooks] = useState<Book[]>(Books);
+  const [booksTemp] = useState<Book[]>(Books);
   const [inputText, setInputText] = useState<string>('');
   const [checkboxes, setCheckboxes] = useState<string[]>([]);
 
   // obliger d'utiliser le useEffect si on veut détecter chaque changement dans le dom
   useEffect(() => {
-   let filter = booksTemp.filter(value => {
-     // si toutes les checkbox sont vide mais qu'on fait une recherche que avec input
-     if(checkboxes.length == 0 && inputText.length >= 0) {
-       return value.author.toLowerCase().includes(inputText.toLowerCase());
-     }
-    //ici on doit gérer si 1 ou plusieurs checkbox sont cliqués mais que input text est vide
-      if(checkboxes.length > 0 && inputText.length == 0) {
-        for (let index = 0; index < checkboxes.length; index++) {
-          if(value.country.toLowerCase() == checkboxes[index].toLowerCase())
-            return (value.country.toLowerCase() == checkboxes[index].toLowerCase());
-        }
-      } 
-      // si une ou plusieurs checkbox sont cliqué et que inputext est pas vide
-      if(checkboxes.length > 0 && inputText.length > 0)  {
-          for (let index = 0; index < checkboxes.length; index++) {
-            if(value.country.toLowerCase() == checkboxes[index].toLowerCase() && 
-            (value.author.toLowerCase().includes(inputText.toLowerCase()))
-            )
-              return (value.country.toLowerCase() == checkboxes[index].toLowerCase())
-              && (value.author.toLowerCase().includes(inputText.toLowerCase()))
-          }
-      }    
-  });
-  
-  filter = filter ? filter : booksTemp;
-  setBooks(filter);
-  
-  }, [checkboxes, inputText])
+    let filter = getFilterBooksArray();
+    setBooks(filter);
+  }, [checkboxes, inputText]);
+ 
+
+  const getFilterBooksArray = () => {
+    let filter = booksTemp.filter(value => {
+      // si toutes les checkbox sont vide mais qu'on fait une recherche que avec input
+      if(checkboxes.length == 0 && inputText.length >= 0) {
+        return searchOnlyFromInput(value);
+      }
+     //ici on doit gérer si 1 ou plusieurs checkbox sont cliqués mais que input text est vide
+       if(checkboxes.length > 0 && inputText.length == 0) {
+        return searchOnlyWithcheckboxes(value);
+       } 
+       // si une ou plusieurs checkbox sont cliqué et que inputext est pas vide
+       if(checkboxes.length > 0 && inputText.length > 0)  {
+           return searchWithcheckboxesAndInput(value);
+       }    
+   });
+   
+  return filter = filter ? filter : booksTemp;
+  }
+
+  const searchOnlyFromInput = (value:any) => {
+    // if(Object.keys(value).some(k => value[k].includes(inputText.toLowerCase()))) {
+    //   console.log(Object.keys(value).some(k => value[k].toLowerCase().includes(inputText.toLowerCase())));
+    // }
+    return value.author.toLowerCase().includes(inputText.toLowerCase());
+  }
+
+  const searchOnlyWithcheckboxes = (value:Book) => {
+    for (let index = 0; index < checkboxes.length; index++) {
+      if(value.country.toLowerCase() == checkboxes[index].toLowerCase())
+        return (value.country.toLowerCase() == checkboxes[index].toLowerCase());
+    }
+  }
+
+  const searchWithcheckboxesAndInput = (value:Book) => {
+    for (let index = 0; index < checkboxes.length; index++) {
+      if(value.country.toLowerCase() == checkboxes[index].toLowerCase() && 
+      (value.author.toLowerCase().includes(inputText.toLowerCase()))
+      )
+        return (value.country.toLowerCase() == checkboxes[index].toLowerCase())
+        && (value.author.toLowerCase().includes(inputText.toLowerCase()))
+    }
+  }
 
   const handleFilterChange = (inputType: string, input:string):void => {
     if(inputType == 'input') {
